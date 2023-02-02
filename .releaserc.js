8618289561d70d9c4ae4709ef942f4a60f422c50
Zhang Rui <zhangrui@growingio.com>
2023-02-02 20:47:53 +0800

    docs: update .releaserc.json for semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -7,11 +7,19 @@ module.exports = {
         config: "conventional-changelog-cmyr-config",
       },
     ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
     "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
-        assets: ["package.json"],
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
     "@semantic-release/github",
